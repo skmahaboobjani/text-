f96024f46d2ddc907082b7de2b4870f23ddf5d6b
Shaik Mahaboob jani <92629496+skmahaboobjani@users.noreply.github.com>
2021-11-14 04:02:28 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function mabu() {
+
+    var f1=document.getElementById("para")
+    f1.style.display = "block";  
+}
+
+function mabu1() {
+
+    var f2=document.getElementById("para1")
+    f2.style.display = "block";  
+}
+
+function mabu2() {
+
+    var f3=document.getElementById("para2")
+    f3.style.display = "block";  
+}
+
+
+
